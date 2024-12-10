ac51b4a3f7f9bb449a3a9f656a96c10a5b24bee5
1naresh <mergu345@gmail.com>
2024-12-10 19:40:45 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,38 +5,33 @@ import "./page.scss"
 
 const { Header,Footer } = Layout;
 
-const contentStyle: React.CSSProperties = {
-  height: "475px",
-  color: "#fff",
-  textAlign: "center",
-  background: "#364d79",
-};
+
 const App: React.FC = () => {
   return (
     <div>
       <Header style={{ display: "flex", alignItems: "center" }}>
         <div className="demo-logo" style={{ height: "64px" }}>
-          <Image style={{ width: "117px" }} src={"/logo.png"} alt="logo" />
+          <Image preview={false} style={{ width: "117px" }} src={"/logo.png"} alt="logo" />
         </div>
         {/* <div style={{color:"#fff"}} >Aloka Engineers</div> */}
       </Header>
       <div style={{ width: "100%" }} className="ae-carousel">
         <Carousel autoplay >
           <div>
-            <div style={contentStyle}>
+            <div className="ae-carousel-img">
               {" "}
-              <Image style={{ width: "100%",transform:"translateY(-181px)" }} src={"/caro2.jpeg"} alt="logo" />
+              <Image preview={false} className="img1" style={{ width: "100%" }} src={"/caro2.jpeg"} alt="logo" />
             </div>
           </div>
           <div>
-            <div style={contentStyle}>
+            <div className="ae-carousel-img">
               {" "}
-              <Image style={{ width: "70%",transform:"translateY(-10px)",margin:"0 15%" }} src={"/caro3.jpeg"} alt="logo" />
+              <Image preview={false} style={{ width: "70%",transform:"translateY(-10px)",margin:"0 15%" }} src={"/caro3.jpeg"} alt="logo" />
             </div>
           </div>
           <div>
-            <div style={contentStyle}>
-              <Image style={{ width: "100%",transform:"translateY(-10px)" }} src={"/caro4.jpeg"} alt="logo" />
+            <div className="ae-carousel-img">
+              <Image preview={false} style={{ width: "100%",transform:"translateY(-10px)" }} src={"/caro4.jpeg"} alt="logo" />
             </div>
           </div>
         </Carousel>
